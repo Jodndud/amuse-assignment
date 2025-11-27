70de84af8dd3686ba345c37abb926884d1d4d0fd
wooyeong <doriconi@gmail.com>
2025-11-28 01:07:00 +0900

    feat: detail페이지 recoil상태 적용

--- src/pages/DeviceDetail.tsx
+++ src/pages/DeviceDetail.tsx
@@ -1,10 +1,21 @@
+import { useParams } from 'react-router-dom'
+import { useRecoilValue } from 'recoil'
+import { deviceByIdState } from '../recoil/devicesState'
+
 import styled from "styled-components";
 import DetailHeader from "@/components/DetailHeader";
 
+
 export default function DeviceDetail(){
+    const { deviceId } = useParams<{ deviceId: string }>()
+
+    const device = useRecoilValue(
+        deviceByIdState(deviceId || ''),
+    )
+
     return(
         <Wrapper>
-            <DetailHeader>기기이름</DetailHeader>
+            <DetailHeader>{device?.name}</DetailHeader>
         </Wrapper>
     )
 }
